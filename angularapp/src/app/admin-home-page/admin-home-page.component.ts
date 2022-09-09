3ea099d7869be7297c24696a5f0bddaa0d39d6b9
shruthivenkat137 <82895321+shruthivenkat137@users.noreply.github.com>
2022-09-09 15:17:36 +0530

    Spring Security

--- angularapp/src/app/admin-home-page/admin-home-page.component.ts
+++ angularapp/src/app/admin-home-page/admin-home-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-home-page.component.css']
 })
 export class AdminHomePageComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit(): void {
